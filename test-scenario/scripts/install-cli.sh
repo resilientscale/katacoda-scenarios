df446683a60aa5875ee2b559328be10f631a5eb5
Steve Greenberg <spgreenberg@gmail.com>
2019-10-05 09:59:39 -0600

    install cli

---
+++ test-scenario/scripts/install-cli.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+curl -L "https://packages.cloudfoundry.org/stable?release=linux64-binary&source=github" | tar -zx
+
+mv cf /usr/local/bin
+
+sudo curl -o /usr/share/bash-completion/completions/cf https://raw.githubusercontent.com/cloudfoundry/cli/master/ci/installers/completion/cf
