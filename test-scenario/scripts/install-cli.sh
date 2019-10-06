8dc973ee84e33e15d6b2fb19678d3dffa7b6088e
Steve Greenberg <spgreenberg@gmail.com>
2019-10-06 07:42:59 -0600

    curl enpdoint during initialization

--- test-scenario/scripts/install-cli.sh
+++ test-scenario/scripts/install-cli.sh
@@ -5,3 +5,5 @@ curl -L "https://packages.cloudfoundry.org/stable?release=linux64-binary&source=
 mv cf /usr/local/bin
 
 sudo curl -o /usr/share/bash-completion/completions/cf https://raw.githubusercontent.com/cloudfoundry/cli/master/ci/installers/completion/cf
+
+curl https://http-dump.cfapps.io
